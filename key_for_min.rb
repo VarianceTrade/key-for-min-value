ffa53f2a03a9fc343f5481a1904f8b9693689d88
Brandon <sugb73@gmail.com>
2021-01-23 13:46:20 -0500

    commit tree 884555ec23ee162f604c25407748a393fa26f27b

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,37 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(hash)
+    min_key = nil
+    min = 1000
+    hash.each do |key, number|
+        if number < min
+            min = number # set min to new smallest number
+            min_key = key
+        else
+            # don't do anything
+        end
+    end
+    min_key
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Don't use:
+#key
+#keys
+#values
+#min
+#sort
+#sort_by
+#min_by
